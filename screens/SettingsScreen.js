895000fbdf0132361ee3e191cec45d7ab61aa18f
maxdtd <max.schroen@gmail.com>
2019-05-09 14:30:02 +0200

    Cleaned up template, added primitive camera

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import { View } from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
